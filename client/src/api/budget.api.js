6021accbd0853e4507eeab5cb62526a24fe809ad
trasvi <trasvib106@gmail.com>
2026-04-20 14:59:51 +0530

    fix budget api export

--- client/src/api/budget.api.js
+++ client/src/api/budget.api.js
@@ -1,13 +1,4 @@
-import API from './axios';
+import API from './axios'
 
-// The primary function used by the Budget Page
-export const getBudget = () => API.get('/api/budget/current');
-
-// ALIAS: This fixes the DashboardPage crash by supporting the old name
-export const getCurrentBudget = getBudget;
-
-// Updates specific budget parameters
-export const updateBudget = (data) => API.put('/api/budget', data);
-
-// The AI Engine trigger
-export const buildBudgetWithAI = () => API.post('/api/budget/generate');
+export const getCurrentBudget = ()     => API.get('/api/budget/current')
+export const generateBudget   = (data) => API.post('/api/budget/generate', data)
